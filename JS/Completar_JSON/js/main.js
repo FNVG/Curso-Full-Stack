d642ba5c9bec84cc36da7502c2dc07e7344d0e34
Florencia Viera <florencialujanvieragaleano@email.com>
2024-07-24 19:19:49 -0400

    Ejercicio de autocompletado

---
+++ JS/Completar_JSON/js/main.js
@@ -0,0 +1,48 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+// Buscar en states.json y filtrar
+const searchStates = async searchText => {
+    console.log(searchText);
+   
+        const res = await fetch('http://127.0.0.1:5500/JS/Completar_JSON/Completar_JSON/data/states.json');
+        
+      
+            const states = await res.json();
+
+            // Obtener coincidencia con el texto de entrada
+            let matches = states.filter(state => {
+                const regex = new RegExp(`^${searchText}`, 'gi');
+      
+                return state.name.match(regex) || state.abbr.match(regex);
+           
+            });
+
+            if (searchText.length == 0) {
+                matches = [];
+                matchList.innerHTML= '';
+            }  
+
+            outputHTML(matches);
+      
+
+};
+
+
+const outputHTML = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+        <div class="card card-body mb-1">
+            <h4>${match.name} (${match.abbr}) <span class="text-primary"
+            ${match.capital}</span></h4>
+        <small>Lat: ${match.lat} / Long: ${match.long} </small>    
+        </div>
+        `).join('');
+        matchList.innerHTML = html;
+    }
+}
+
+
+search.addEventListener('input', () => searchStates(search.value));
+
+
